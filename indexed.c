0c981295bbe12d48428a89714424a1d6192db8b5
Qi Rong Kwang <1901829@sit.singaporetech.edu.sg>
2020-02-02 23:27:06 +0800

    Update indexed.c

--- indexed.c
+++ indexed.c
@@ -1,12 +1,28 @@
 #include "indexed.h"
+#include "readCSV.c"
 
 void indexed(){
-    //Block number 0 will be our index block. So nodeSize in Block 0 will store the pointer to the various file.
-    int j =0;
-    // printf("Inside indexed.c \n");
-    // for (j=0;j<MAX_BLOCK; j++){
-    //     printf("%d",node[j].index);
-    // }
+    //Node 0 will be our index block
+    int j;
+    printf("Inside indexed.c \n");
+    for (j=1;j<MAX_BLOCK; j++){
+        if (strlen(node[j].data) == 0)
+        {
+            strcpy(node[j].data, "");
+        }
+    printf("Node Data: %s\n",node[j].data);
+
+    }
+
+    //1. Find out how to get array from readCSV
+    //2. Once can read array, extract out file Num and file Data numbers
+    //3. Find once find empty node, store file data and tag file variable for node.
+
     
+    // for (j=0; j<50; j++){
+    //     printf("CSV array: %s\n", CSV_Data[j]);
+
+    // }
+
 
 }
