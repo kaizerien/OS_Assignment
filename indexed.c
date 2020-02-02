3e51df42908a55a94858c806dbd4dde910d0882a
Qi Rong Kwang <1901829@sit.singaporetech.edu.sg>
2020-02-02 22:25:02 +0800

    Update indexed.c

--- indexed.c
+++ indexed.c
@@ -1,6 +1,7 @@
 #include "indexed.h"
 
 void indexed(){
+    //Block number 0 will be our index block. So nodeSize in Block 0 will store the pointer to the various file.
     int j =0;
     // printf("Inside indexed.c \n");
     // for (j=0;j<MAX_BLOCK; j++){
